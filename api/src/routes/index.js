3f36dad2cf375fa2523a950fabcb4e84e993f146
Alejandro Centeno <alejocm21@gmail.com>
2023-02-14 17:49:02 -0300

    save types of pokemons on database

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 const { getPokemonController, getPokemonsByIdController } = require('../controllers/pokemonControllers');
 const savePokemonController = require('../controllers/savePokemonController');
+const getTypesPokemonController = require('../controllers/typesPokemonControllers')
 
 
 const router = Router();
@@ -11,8 +12,9 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 router.get('/pokemons', getPokemonController);
-router.get('/pokemons/:id', getPokemonsByIdController)
-router.post('/pokemons', savePokemonController)
+router.get('/pokemons/:id', getPokemonsByIdController);
+router.post('/pokemons', savePokemonController);
+router.get('/types', getTypesPokemonController);
 
 
 module.exports = router;
