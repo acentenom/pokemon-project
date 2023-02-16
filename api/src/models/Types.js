a243729b0004ad558de05ea8f3ffcd88bc84fe05
Alejandro Centeno <106875806+acentenom@users.noreply.github.com>
2023-02-16 18:01:42 -0300

    Revert "Feature/get all pokemon"

--- api/src/models/Types.js
+++ api/src/models/Types.js
@@ -4,6 +4,12 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('type', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,20 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const { getPokemonController, getPokemonsByIdController } = require('../controllers/pokemonControllers');
-const savePokemonController = require('../controllers/savePokemonController');
-const getTypesPokemonController = require('../controllers/typesPokemonControllers')
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-router.get('/pokemons', getPokemonController);
-router.get('/pokemons/:id', getPokemonsByIdController);
-router.post('/pokemons', savePokemonController);
-router.get('/types', getTypesPokemonController);
 
 
 module.exports = router;
