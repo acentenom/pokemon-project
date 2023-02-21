aa946a537f032aeb06a1b8e270c7008655ab012e
Alejandro Centeno <alejocm21@gmail.com>
2023-02-21 11:33:10 -0300

    lading-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { Route, Switch, BrowserRouter } from "react-router-dom"
 import Home from './components/Home/Home.jsx';
+import LandingPage from './components/LandingPage/LandingPage';
 
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>Henry Pokemon</h1>
       <Switch>
-        <Route path="/home">
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route exact path="/home">
           <Home />
         </Route>
       </Switch>
