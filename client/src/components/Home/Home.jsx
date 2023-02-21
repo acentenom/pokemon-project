cb02101e6bc52016ad3baab8602f5436537364e0
Alejandro Centeno <alejocm21@gmail.com>
2023-02-21 14:56:58 -0300

    paging function

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,35 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getPokemons } from "../../redux/action";
 import CardPokemon from "../CardPokemon/CardPokemon";
-import style from "../Home/home.module.css"
-
+import style from "../Home/home.module.css";
+import Paginated from "../Paginated/Paginated";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const allPokemons = useSelector((state) => state.pokemons);
 
-    const dispatch = useDispatch();
-    const allPokemons = useSelector((state => state.pokemons))
+  /* Paginado */
 
-useEffect(() => {
-    dispatch(getPokemons())
-},[dispatch])
+  const [paginado, setPaginado] = useState(1);
+  const [porPagina, setPorPagina] = useState(12);
 
-    return (
-        <div className={style.fondo}>
-            <Link to='/pokemon'>Create Pokemon</Link>
-            <div className={style.containerCard}>
-                {allPokemons?.map(p => {
-                    return (
-                    <div>
-                        <div>
-                    <CardPokemon key={p.id} name={p.name} image={p.image} type={p.type} />
-                    </div>
-                       </div>
-                )})}
+  const ultimoPoke = paginado * porPagina;
+  const primerPoke = ultimoPoke - porPagina;
+  const pokePagina = allPokemons.slice(primerPoke, ultimoPoke);
+  const paginadoDePokemon = (pagenumber) => {
+    setPaginado(pagenumber);
+  };
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, [dispatch]);
+
+  return (
+    <div className={style.fondo}>
+      <Link to="/pokemon">Create Pokemon</Link>
+      <div className={style.containerCard}>
+        {pokePagina?.map((p) => {
+          return (
+            <div>
+              <div>
+                <CardPokemon
+                  key={p.id}
+                  name={p.name}
+                  image={p.image}
+                  type={p.type}
+                />
+              </div>
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+      <Paginated
+        porPagina={porPagina}
+        allPokemons={allPokemons.length}
+        paginadoDePokemon={paginadoDePokemon}
+      />
+    </div>
+  );
+};
 
 export default Home;
---
+++ client/src/components/Paginated/Paginated.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Paginated = ({ porPagina, paginadoDePokemon, allPokemons }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(allPokemons / porPagina); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav>
+      <ul>
+        {pageNumbers &&
+          pageNumbers.map((num) => (
+            <li>
+              <button onClick={() => paginadoDePokemon(num)}>{num}</button>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Paginated;
