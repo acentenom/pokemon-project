6ba90db8336a1ce4170f901cccd6a9dad11f15e0
Alejandro Centeno <alejocm21@gmail.com>
2023-02-24 16:29:14 -0300

    filter by origin: API or Data Base

--- client/src/redux/action/index.js
+++ client/src/redux/action/index.js
@@ -5,6 +5,7 @@ export const GET_SEARCH = "GET_SEARCH";
 export const GET_DETAIL = "GET_DETAIL";
 export const POST_CREATE_POKEMON = "POST_CREATE_POKEMON";
 export const GET_TYPES = "GET_TYPES";
+export const FILTER_FOR_ORIGIN = "FILTER_FOR_ORIGIN";
 
 export const getPokemons = () => {
   return async function (dispatch) {
@@ -15,7 +16,7 @@ export const getPokemons = () => {
       return dispatch({ type: GET_ALL_POKEMONS, payload: pokemon.data });
     } catch (error) {
       throw error;
-    }
+    };
   };
 };
 
@@ -26,8 +27,8 @@ export const getSearchPokemon = (name) => {
       return dispatch({ type: GET_SEARCH, payload: search.data })
     } catch (error) {
       throw error;
-    }
-  }
+    };
+  };
 };
 
 export const getPokemonDetail = (id) => {
@@ -37,8 +38,8 @@ export const getPokemonDetail = (id) => {
       return dispatch({ type: GET_DETAIL, payload: detail.data });
     } catch (error) {
       throw error;
-    }
-  }
+    };
+  };
 };
 
 export const createPokemon = (body) => {
@@ -48,13 +49,20 @@ export const createPokemon = (body) => {
       return create;
     } catch (error) {
       throw error;
-    }
-  }
+    };
+  };
 };
 
 export const getTypes = () => {
   return async function (dispatch) {
     const types = await axios.get(`http://localhost:3001/types`)
     return dispatch({ type: GET_TYPES, payload: types.data })
-  }
-}
+  };
+};
+
+export const fitlerForOrigin = (payload) => {
+  return {
+    type: FILTER_FOR_ORIGIN,
+    payload
+  };
+};
